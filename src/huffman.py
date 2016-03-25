16a5dc1f88d203ba42f69d672b33a39ebb5393a3
Luiz Oliveira <ziuloliveira@gmail.com>
2016-03-25 03:01:40 -0300

    It's  a object now!
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

--- src/huffman.py
+++ src/huffman.py
@@ -12,19 +12,44 @@ from collections import defaultdict
 from huffargs import _parser
 
 
-def encode(symb2freq):
-    """Huffman encode the given dict mapping symbols to weights."""
-    heap = [[wt, [sym, ""]] for sym, wt in symb2freq.items()]
-    heapify(heap)
-    while len(heap) > 1:
-        lo = heappop(heap)
-        hi = heappop(heap)
-        for pair in lo[1:]:
-            pair[1] = '0' + pair[1]
-        for pair in hi[1:]:
-            pair[1] = '1' + pair[1]
-        heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
-    return sorted(heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
+class HuffmanCompactor(object):
+    """docstring for Huffman Compactor."""
+
+    def __init__(self, filename):
+        """Initialize the object."""
+        super(HuffmanCompactor, self).__init__()
+        self.filename = filename
+        txt = open(self.filename, "r").read()
+        self.symb2freq = defaultdict(int)
+        for ch in txt:
+            self.symb2freq[ch] += 1
+        if not len(self.symb2freq):
+            raise IndexError('Input is empty, no magic here...')
+
+    def encode(self):
+        """Huffman encode the given dict mapping symbols to weights."""
+        heap = [[wt, [sym, ""]] for sym, wt in self.symb2freq.items()]
+        heapify(heap)
+        while len(heap) > 1:
+            lo = heappop(heap)
+            hi = heappop(heap)
+            for pair in lo[1:]:
+                pair[1] = '0' + pair[1]
+            for pair in hi[1:]:
+                pair[1] = '1' + pair[1]
+            heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
+        self.table = sorted(heappop(heap)[1:], key=lambda p: (len(p[-1]), p))
+        return self.table
+
+    def __str__(self):
+        """Format the output preatty."""
+        out = "Symbol       Weight\tHuffman Code\n"
+        for p in self.table:
+            # print "%s\t\t%s\t%s" % ([p[0]], symb2freq[p[0]], p[1])
+            out += '\r{}'.format([p[0]]).ljust(13)
+            out += '{}\t'.format(self.symb2freq[p[0]]).rjust(5)
+            out += '{}\n'.format(p[1]).ljust(20)
+        return out
 
 
 def main():
@@ -36,22 +61,9 @@ def main():
         else:
             _parser.print_help()
             return
-    txt = open(_options.filename, "r").read()
+    huff = HuffmanCompactor(_options.filename)
+    huff.encode()
+    print(huff)
 
-    symb2freq = defaultdict(int)
-    for ch in txt:
-        symb2freq[ch] += 1
-    if not len(symb2freq):
-        print 'Input is empty, no magic here...'
-        return
-    # in Python 3.1+:
-    # symb2freq = collections.Counter(txt)
-    huff = encode(symb2freq)
-    print "Symbol       Weight\tHuffman Code"
-    for p in huff:
-        # print "%s\t\t%s\t%s" % ([p[0]], symb2freq[p[0]], p[1])
-        print '{}'.format([p[0]]).ljust(13),
-        print '{}\t'.format(symb2freq[p[0]]).rjust(5),
-        print '{}'.format(p[1]).ljust(20)
 if __name__ == '__main__':
     main()
